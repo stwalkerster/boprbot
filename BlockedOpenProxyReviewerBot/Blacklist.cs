162c0c0b3f6646ed31870d48a7795833beabb965
Simon Walker <github@helpmebot.org.uk>
2010-01-30 20:30:56 +0000

    git-svn-id: https://svn.toolserver.org/svnroot/stwalkerster/trunk/BlockedOpenProxyReviewerBot@425 9e49ee4e-5cec-4418-9268-a288311b67ef

---
+++ BlockedOpenProxyReviewerBot/Blacklist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+namespace BlockedOpenProxyReviewerBot
+{
+    class Blacklist
+    {
+        public Blacklist( IPAddress openProxyResult, string zoneSuffix )
+        {
+            result = openProxyResult;
+            suffix = zoneSuffix;
+        }
+
+        IPAddress result;
+        string suffix;
+        
+
+        
+
+        public bool openProxy( IPAddress ip )
+        {
+
+        }
+    }
+}
--- BlockedOpenProxyReviewerBot/Program.cs
+++ BlockedOpenProxyReviewerBot/Program.cs
@@ -9,6 +9,37 @@ namespace BlockedOpenProxyReviewerBot
     {
         static void Main( string[ ] args )
         {
+            new Program( args );
+        }
+
+        public Program( string[] args )
+        {
+            if( args.Length != 5 )
+            {
+                showHelp( );
+            }
+            else
+            {
+                db = new Database( args[ 0 ], (uint)args[ 1 ], args[ 4 ], args[ 2 ], args[ 3 ] );
+                runBot( );
+            }
+        }
+
+        Database db;
+
+        void showHelp( )
+        {
+            Console.WriteLine( "Usage: mono bopr.exe <server> <port> <username> <password> <schema>" );
+        }
+
+        void runBot( )
+        {
+            Block[ ] proxyBlocks = db.getProxyBlocks( );
+
+            foreach( Block b in proxyBlocks )
+            {
+                
+            }
         }
     }
 }
