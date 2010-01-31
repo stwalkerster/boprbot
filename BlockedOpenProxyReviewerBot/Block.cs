7d05609742ad7d52e1299a47a9e215c09d9c2af2
Simon Walker <github@helpmebot.org.uk>
2010-01-31 23:04:36 +0000

    git-svn-id: https://svn.toolserver.org/svnroot/stwalkerster/trunk/BlockedOpenProxyReviewerBot@428 9e49ee4e-5cec-4418-9268-a288311b67ef

--- BlockedOpenProxyReviewerBot/Block.cs
+++ BlockedOpenProxyReviewerBot/Block.cs
@@ -28,13 +28,23 @@ namespace BlockedOpenProxyReviewerBot
         public static Block newFromDataRow( object[ ] values )
         {
             Block b = new Block( );
+
             b.ipb_id = (int)( values[ 0 ] );
-
-
-            ipb_address = Utility.Net.IPAddress.newFromEncodedString( values[ 1 ] );
-
-
-
+            b.ipb_address = Utility.Net.IPAddress.newFromEncodedString( (byte[ ])values[ 1 ] );
+            b.ipb_user = (int)values[ 2 ];
+            b.ipb_by = (int)values[ 3 ];
+            b.ipb_reason = ASCIIEncoding.ASCII.GetString( (byte[ ])values[ 4 ] );
+            b.ipb_timestamp = new MySql.Data.Types.MySqlDateTime( ASCIIEncoding.ASCII.GetString( (byte[ ])values[ 5 ] ) ).GetDateTime( );
+            b.ipb_auto = ( (int)values[ 6 ] ) == 1 ? true : false;
+            b.ipb_anon_only = ( (int)values[ 7 ] ) == 1 ? true : false;
+            b.ipb_create_account = ( (int)values[ 8 ] ) == 1 ? true : false;
+            b.ipb_expiry = ASCIIEncoding.ASCII.GetString( (byte[ ])values[ 9 ] );
+            b.ipb_range_start = Utility.Net.IPAddress.newFromEncodedString( (byte[ ])values[ 10 ] );
+            b.ipb_range_end = Utility.Net.IPAddress.newFromEncodedString( (byte[ ])values[ 11 ] );
+            b.ipb_enable_autoblock = ( (int)values[ 12 ] ) == 1 ? true : false;
+            b.ipb_deleted = ( (int)values[ 13 ] ) == 1 ? true : false;
+            b.ipb_block_email = ( (int)values[ 14 ] ) == 1 ? true : false;
+            b.ipb_by_text = ASCIIEncoding.ASCII.GetString( (byte[ ])values[ 15 ] );
 
             return b;
         }
