aadd338a5a978f6ffa8e8bda7ad911dbec2cd07b
Simon Walker <github@helpmebot.org.uk>
2010-02-01 04:41:35 +0000

    git-svn-id: https://svn.toolserver.org/svnroot/stwalkerster/trunk/BlockedOpenProxyReviewerBot@430 9e49ee4e-5cec-4418-9268-a288311b67ef

--- BlockedOpenProxyReviewerBot/Program.cs
+++ BlockedOpenProxyReviewerBot/Program.cs
@@ -21,15 +21,28 @@ namespace BlockedOpenProxyReviewerBot
             else
             {
                 db = new Database( args[ 0 ], uint.Parse(args[ 1 ]), args[ 4 ], args[ 2 ], args[ 3 ] );
+                DNSBL = initialiseBlacklists( );
                 runBot( );
             }
         }
 
         Database db;
+        Blacklist[ ] DNSBL;
 
         void showHelp( )
         {
             Console.WriteLine( "Usage: [mono] bopr.exe <server> <port> <username> <password> <schema>" );
+        }
+
+        Blacklist[ ] initialiseBlacklists( )
+        {
+            Blacklist[ ] bl = new Blacklist[ 5 ];
+            bl[ 0 ] = new Blacklist( Utility.Net.IPAddress.newFromString( "127.0.0.9" ), "dnsbl.njabl.org" );
+            bl[ 1 ] = new Blacklist( Utility.Net.IPAddress.newFromString( "127.0.0.2" ), "dnsbl.proxybl.org" );
+            bl[ 2 ] = new Blacklist( Utility.Net.IPAddress.newFromString( "127.0.0.2" ), "http.dnsbl.sorbs.net" );
+            bl[ 3 ] = new Blacklist( Utility.Net.IPAddress.newFromString( "127.0.0.3" ), "socks.dnsbl.sorbs.net" );
+            bl[ 4 ] = new Blacklist( Utility.Net.IPAddress.newFromString( "127.0.0.4" ), "misc.dnsbl.sorbs.net" );
+            return bl;
         }
 
         void runBot( )
